87d8af32f628b72d59549670252207838d63cbf0
freedom <freedom.chuks7@gmail.com>
2019-10-10 09:11:19 -0700

    fixed equal changes in recyclerview

--- app/src/main/java/com/freedom/notey/db/Note.kt
+++ app/src/main/java/com/freedom/notey/db/Note.kt
@@ -3,7 +3,7 @@ package com.freedom.notey.db
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import java.io.Serializable
-
+import java.time.LocalDateTime
 
 
 //Todo this is an abstraction layer to create sqlite table
@@ -12,7 +12,10 @@ import java.io.Serializable
 data class Note(
     val Title:String?=null,//column 2
     val Note:String?=null// column 3
-//    val Date:LocalDate
+//    val modifiedAt:LocalDateTime,
+//    val createdAt:LocalDateTime,
+//    val pin:Int
+
 ):Serializable{
     @PrimaryKey(autoGenerate = true)
     var id:Int=0
